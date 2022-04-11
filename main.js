84cf3cac953dc7b348b4c854931013a23c0aa058
Daniel Ting <danielzting@gmail.com>
2022-04-10 23:08:05 -0500

    Add PWA, A2HS and offline support

--- main.js
+++ main.js
@@ -195,3 +195,9 @@ function shuffle(array) {
     }
     return array;
 }
+
+window.addEventListener('load', () => {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('service-worker.js');
+    }
+});
